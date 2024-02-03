6aedb10d46a594a058a541f3250726230a4fbd09
hillerliao <hillerliao@163.com>
2024-02-03 12:40:19 +0900

    add pelican-webring requirement statement

--- pelicanconf.py
+++ pelicanconf.py
@@ -46,8 +46,7 @@ DEFAULT_PAGINATION = False
 WEBRING_FEED_URLS = [
     'https://flomo.liaozh.top/u/101/rss.xml',
     'https://rsshub.app/telegram/channel/JapanCoder',
-    'https://nitter.net/hillerliao/rss',
-    'https://rss.smallmaple.com/rss/user/1281593140'
+    'https://nitter.1d4.us/hillerliao/rss'
 ]
 WEBRING_ARTICLES_PER_FEED = 3000
 WEBRING_MAX_ARTICLES = 6000
